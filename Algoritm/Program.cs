361f0f2d9f59bb1c15b36a70ca2f3e987706ea47
Ruben2412 <114781107+Ruben2412@users.noreply.github.com>
2022-12-22 03:01:55 +0500

    ThreeWays

--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -22,29 +22,100 @@ string InputText(string input)
     return output;
 }
 
-string [] CreateSizeArray(int size)
+string[] CreateSizeArray(int size)
 {
     string[] array = new string[size];
 
     for (int i = 0; i < size; i++)
     {
+        Console.WriteLine();
         var element = InputText($"Input {i + 1} element: ");
         array[i] = element;
-        
+
     }
     return array;
 }
 
-void ShowArray(string [] array)
+string[] ChangeCreatedArray(string[] array, int size)
 {
-    for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + "; ");
-    Console.WriteLine();
+    string[] massiv = new string[size];
+
+    int length = 3;
+    int j = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i].Length <= length)
+        {
+            massiv[j] = array[i];
+            j++;
+        }
+    }
+    return massiv;
 }
 
+string[] CutCreatedArray(string[] array, int size, int length)
+{
+    string[] massiv = new string[size];
 
-int number = InputNumbers("Imput count of elements ");
+    int j = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i].Length <= length)
+        {
+            massiv[j] = array[i];
+            j++;
+        }
+    }
+    return massiv;
+}
+
+void ShowArray(string[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "; ");
+        Console.WriteLine();
+    }
+}
+
+void ShowThirdLetterArray(string[] array)
+{
+    Console.WriteLine();
+    int length = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length)
+        {
+            Console.Write(array[i] + "; ");
+            Console.WriteLine();
+        }
+    }
+}
+
+int number = InputNumbers("Enter the number of array elements: ");
 
 string[] arrayString = CreateSizeArray(number);
 
+Console.WriteLine();
+Console.WriteLine("The first is the most boring option");
+
 ShowArray(arrayString);
+ShowThirdLetterArray(arrayString);
+
+Console.WriteLine();
+Console.WriteLine("The second option is more interesting");
+
+ShowArray(arrayString);
+string[] changedArrayString = ChangeCreatedArray(arrayString, number);
+ShowArray(changedArrayString);
+
+
+Console.WriteLine();
+Console.WriteLine("The third option is where you can determine the number of elements in an array cell");
+int leng = InputNumbers("Enter the number of elements in the array cell: ");
+
+ShowArray(arrayString);
+string[] cutArrayString = CutCreatedArray(arrayString, number, leng);
+ShowArray(cutArrayString);
